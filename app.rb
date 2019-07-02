21615b1d28021d9c8fbbb45b9f20fbf765bfe1bc
Michael Mottola <mottola64@gmail.com>
2019-07-02 01:59:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ class App < Sinatra::Base
   end
 
   get '/set' do
+    # set the :foo key of the session hash equal to 'hello' here!
     session[:foo] = "hello"
     if session[:foo] == 'hello'
       redirect '/fetch'
@@ -32,29 +33,29 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :user_id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:user_id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
-      session[:user_id] == 1
-        #set session id here
-        if session[:user_id] == 1
-          # "Session ID set. It's currently set to #{session[:user_id]}."
-          redirect '/fetch_session_id'
-        else
-          "Session ID has not been set!"
-        end
-      end
+    session[:user_id] = 1
+    #set session id here
+    if session[:user_id] == 1
+      # "Session ID set. It's currently set to #{session[:user_id]}."
+      redirect '/fetch_session_id'
+    else
+      "Session ID has not been set!"
+    end
+  end
 
   get '/fetch_session_id' do
-    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:user_id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
-  session = {}
+    session = {}
 
-  "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
-end
+    "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
+  end
 
   get '/finish' do
     "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
